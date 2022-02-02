00dd4ee12eb2d9cbb7a951f6ff3818e971e26fde
rubikkubika <artw123@mail.ru>
2022-02-02 17:53:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.UnaryOperator;
+
+public class Main {
+
+
+    public static void main(String[] Args) throws IOException {
+        System.out.println("Hello world!");
+    }
+
+
+}
